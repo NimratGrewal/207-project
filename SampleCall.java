48d38b388f0bfdb744b93e064622e97304fa0021
momoasalu <motunrayoasalu@gmail.com>
2023-09-29 18:48:01 -0400

    Create constants for client id and client secret

---
+++ SampleCall.java
@@ -0,0 +1,17 @@
+import okhttp3.*;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class SampleCall {
+    //Get Client ID and Client Secret
+    private static final String CLIENT_ID = "216b6438ba554128ade0b63afa48ddd8";
+    private static final String CLIENT_SECRET = "56e33aff04bc4aa28dc3d9e54bb231cd";
+
+    public static void main(String[] args) {
+    }
+}
