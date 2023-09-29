d03f777296d686f725a742d5d47dd7c263ea8017
momoasalu <motunrayoasalu@gmail.com>
2023-09-29 18:49:00 -0400

    Get an access token from an API call

--- SampleCall.java
+++ SampleCall.java
@@ -13,5 +13,25 @@ public class SampleCall {
     private static final String CLIENT_SECRET = "56e33aff04bc4aa28dc3d9e54bb231cd";
 
     public static void main(String[] args) {
+        String accessToken;
+
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        RequestBody body = RequestBody.create(String.format("grant_type=client_credentials&client_id=%s&client_secret=%s", CLIENT_ID, CLIENT_SECRET), mediaType);
+        Request request = new Request.Builder()
+                .url("https://accounts.spotify.com/api/token")
+                .post(body)
+                .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            JSONObject responseBody = new JSONObject(response.body().string());
+
+            accessToken = responseBody.getString("access_token");
+        } catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
