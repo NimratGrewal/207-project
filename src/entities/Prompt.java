e99b09a7141835b58f02e2f10835ece109a2789b
Jawad Chowdhury <jawad.chowdhury@mail.utoronto.ca>
2023-11-09 21:31:18 -0500

    - Basic implementation of Prompt class - added in constructor method which initializes the prompt question, the unique prompt id, a boolean value which determines whether the prompt is active or not, and a responses Hashmap which shows all users response to the prompt
    - implemented getter methods, and some setter methods.

--- src/entities/Prompt.java
+++ src/entities/Prompt.java
@@ -1,4 +1,43 @@
 package entities;
+import java.util.UUID;
+import java.util.HashMap;
+
 
 public class Prompt {
+    private final UUID promptid;
+    public final String promptQuestion;
+
+    private boolean isActive;
+
+    private HashMap <UUID, Response> responses;
+    public Prompt(String promptQuestion) {
+        this.promptQuestion = promptQuestion;
+        this.promptid = UUID.randomUUID();
+        this.isActive = false;
+        this.responses = new HashMap<UUID, Response>();
+    }
+
+    public UUID getPromptId() {
+        return promptid;
+    }
+
+    public boolean isActive(){
+        return isActive;
+    }
+
+    public void setActive(boolean active){
+        isActive = active;
+    }
+
+    public String getPromptText(){
+        return promptQuestion;
+    }
+    public HashMap <UUID, Response>  viewPromptResponse(){
+        return responses;
+    }
+    public void setResponses(UUID uuid, Response response){
+        if (!responses.containsKey(uuid)) {
+            responses.put(uuid, response);
+        }
+    }
 }
