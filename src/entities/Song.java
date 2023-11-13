15f907defbe97d4ffa433e37f69663a416484fc0
momoasalu <motunrayoasalu@gmail.com>
2023-11-13 15:59:27 -0500

    Added Javadoc for methods & changed artists field to a List

--- src/entities/Song.java
+++ src/entities/Song.java
@@ -1,38 +1,68 @@
 package entities;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class Song {
     private final String songId;
     private final String name;
-    private final String artist;
+    private final List<String> artists;
     private final String album;
     private final LocalDate releaseDate;
 
-    public Song(String songId, String name, String artist, String album, LocalDate releaseDate) {
+    /**
+     * Creates an instance of Song
+     *
+     * @param songId the Spotify ID of the song
+     * @param name the title of the song
+     * @param artists the performing artists on the song
+     * @param album the album on which the song appears
+     * @param releaseDate the release date of the album the song is on
+     */
+    public Song(String songId, String name, List<String> artists, String album, LocalDate releaseDate) {
         this.songId = songId;
         this.name = name;
-        this.artist = artist;
+        this.artists = artists;
         this.album = album;
         this.releaseDate = releaseDate;
     }
 
+    /**
+     *
+     * @return the Spotify ID of this song
+     */
     public String getSongId() {
         return songId;
     }
 
-    public String getArtist() {
-        return artist;
+    /**
+     *
+     * @return list of names of the performing artists on this Song
+     */
+    public List<String> getArtists() {
+        return artists;
     }
 
+    /**
+     *
+     * @return name of this Song
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     *
+     * @return name of the album this Song is on
+     */
     public String getAlbum() {
         return album;
     }
 
+    /**
+     *
+     * @return release date of this song
+     */
     public LocalDate getReleaseDate() {
         return releaseDate;
     }
