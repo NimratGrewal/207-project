1b5b1e87d98b8213c4776101724aaf394b2ab7ad
Nimrat Grewal <114554275+NimratGrewal@users.noreply.github.com>
2023-11-17 22:16:01 -0500

    added interface adapter, user case foler and added a contructor to CommonUser

--- src/entities/CommonUserFactory.java
+++ src/entities/CommonUserFactory.java
@@ -1,4 +1,12 @@
 package entities;
 
-public class CommonUserFactory {
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class CommonUserFactory implements UserFactory{
+
+    @Override
+    public User create(UUID userId, String name, String password, LocalDateTime creationTime) {
+        return new CommonUser(userId, name, password, creationTime);
+    }
 }
