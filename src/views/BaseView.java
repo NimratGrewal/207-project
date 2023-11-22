e4ec07c98a57e8a13f2fe592e0d2e6c345506bdb
Haya Tariq <ha.tariq@mail.utoronto.ca>
2023-11-22 23:51:01 +0300

    baseview restructued

--- src/views/BaseView.java
+++ src/views/BaseView.java
@@ -2,29 +2,30 @@ package views;
 
 import javax.swing.*;
 
-public class BaseView extends JFrame {
-    private JTabbedPane tabbedPane;
-    public BaseView() {
-        tabbedPane = new JTabbedPane();
-
-        FeedView feedPage = new FeedView();
-        ProfileView profilePage = new ProfileView();
-
-        tabbedPane.addTab("Home", null);
-        tabbedPane.addTab("Feed", null, feedPage);
-        tabbedPane.addTab("Profile", null, profilePage);
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 300);
-        setLocationRelativeTo(null);
-
-        add(tabbedPane);
+public class BaseView extends JTabbedPane {
+    public BaseView(FeedView feedPage, ProfileView profilePage) {
+        addTab("Home", null);
+        addTab("Feed", null, feedPage);
+        addTab("Profile", null, profilePage);
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            BaseView base = new BaseView();
-            base.setVisible(true);
+            FeedView pageOne = new FeedView();
+            ProfileView pageTwo = new ProfileView();
+
+            BaseView tabbedPage = new BaseView(pageOne, pageTwo);
+
+            // Set up the main JFrame
+            JFrame frame = new JFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(400, 300);
+            frame.setLocationRelativeTo(null);
+
+            // Add the tabbedPane to the JFrame
+            frame.add(tabbedPage);
+
+            frame.setVisible(true);
         });
     }
 
