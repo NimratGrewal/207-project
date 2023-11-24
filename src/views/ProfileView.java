3cf66f8963f5a7f846c4d62d1c80b2e65981d117
Nimrat Grewal <114554275+NimratGrewal@users.noreply.github.com>
2023-11-23 20:20:45 -0500

    added (i think these lines got deleted with the previous push?) some lines that were there previously in ProfileView.java

--- src/views/ProfileView.java
+++ src/views/ProfileView.java
@@ -4,16 +4,23 @@ import javax.swing.*;
 import java.awt.*;
 
 import interface_adapter.delete.DeleteController;
+import interface_adapter.delete.DeleteState;
+import interface_adapter.delete.DeleteViewModel;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 public class ProfileView extends JPanel implements ActionListener, PropertyChangeListener {
     private final DeleteController deleteController;
+    private final JButton delete;
+    private final DeleteViewModel deleteViewModel;
 
-    public ProfileView(DeleteController deleteController) {
+    public ProfileView(DeleteController deleteController, DeleteViewModel deleteViewModel) {
         this.deleteController = deleteController;
-
+        this.deleteViewModel = deleteViewModel;
+        this.deleteViewModel.addPropertyChangeListener(this);
       
         setLayout(new BorderLayout());
 
@@ -58,28 +65,27 @@ public class ProfileView extends JPanel implements ActionListener, PropertyChang
         add(contentPanel, BorderLayout.CENTER);
       
         JPanel buttons = new JPanel();
-        delete = new JButton(deleteViewModel.DELETE_BUTTON_LABEL);
+        delete = new JButton(DeleteViewModel.DELETE_BUTTON_LABEL);
         buttons.add(delete);
 
         delete.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if (e.getSource().equals(delete)) {
-                    DeleteState state = deleteViewModel.getState();
+                    DeleteState state = ProfileView.this.deleteViewModel.getState();
                     int dialogButton = JOptionPane.YES_NO_OPTION;
                     int dialogueResult = JOptionPane.showConfirmDialog(delete,
                             "Are you sure you want to delete Response: " + state.getResponseId() + "?", "Warning", dialogButton);
 
                     if (dialogueResult == JOptionPane.YES_OPTION) {
-                        DeleteState deleteState = deleteViewModel.getState();
+                        DeleteState deleteState = ProfileView.this.deleteViewModel.getState();
                         deleteController.execute(deleteState.getResponseId());
                     }
                 }
             }
         });
     }
-  
-    }
+
     private JPanel createAnswerPanel(String answerText) {
         JPanel answerPanel = new JPanel();
         answerPanel.setBackground(Color.lightGray);
@@ -97,3 +103,10 @@ public class ProfileView extends JPanel implements ActionListener, PropertyChang
         DeleteState state = (DeleteState) evt.getNewValue();
         JOptionPane.showConfirmDialog(this, "Response" + state.getResponseId() + "was deleted!");
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
+
