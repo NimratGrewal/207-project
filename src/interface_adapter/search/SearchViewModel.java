d7675f1dfc49acdb6c57deac6e015e44505db7ee
momoasalu <motunrayoasalu@gmail.com>
2023-11-26 01:43:03 -0500

    Create basic elements of SearchView

--- src/interface_adapter/search/SearchViewModel.java
+++ src/interface_adapter/search/SearchViewModel.java
@@ -6,6 +6,8 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 public class SearchViewModel extends ViewModel {
+    public static final String TITLE_LABEL = "search";
+    public static final String SET_RESPONSE_LABEL = "set response";
     private SearchState state;
     public SearchViewModel() { super("search"); }
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
--- src/views/SearchView.java
+++ src/views/SearchView.java
@@ -1,18 +1,37 @@
 package views;
 
 import interface_adapter.search.SearchState;
+import interface_adapter.search.SearchViewModel;
+import interface_adapter.search_tracks.SearchTracksController;
 import interface_adapter.search_tracks.SearchTracksState;
+import interface_adapter.search_tracks.SearchTracksViewModel;
+import views.components.SearchResultBox;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
-public class SearchView extends JPanel implements ActionListener, PropertyChangeListener {
-    @Override
-    public void actionPerformed(ActionEvent e) {
+public class SearchView extends JPanel implements PropertyChangeListener {
+    private final SearchViewModel searchViewModel;
+    private final SearchTracksController searchTracksController;
+    private final SearchTracksViewModel searchTracksViewModel;
 
+    private JLabel promptText;
+    private JList<SearchResultBox> searchResults;
+    public SearchView (SearchViewModel searchViewModel,
+                       SearchTracksController searchTracksController,
+                       SearchTracksViewModel searchTracksViewModel) {
+        this.searchViewModel = searchViewModel;
+        this.searchViewModel.addPropertyChangeListener(this);
+
+        this.searchTracksController = searchTracksController;
+        this.searchTracksViewModel = searchTracksViewModel;
+        this.searchTracksViewModel.addPropertyChangeListener(this);
+
+        promptText = new JLabel(SearchViewModel.TITLE_LABEL);
+        JTextField searchBarField = new JTextField();
+        searchResults = new JList<>();
+        JButton setResponse = new JButton(SearchViewModel.SET_RESPONSE_LABEL);
     }
 
     @Override
