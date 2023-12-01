385847c9bc5000c349aa95c74262e4a70344cbb7
Nimrat Grewal <114554275+NimratGrewal@users.noreply.github.com>
2023-11-30 20:34:10 -0500

    finished up view response view use case factory

--- src/app/ViewResponseViewUseCaseFactory.java
+++ src/app/ViewResponseViewUseCaseFactory.java
@@ -4,23 +4,38 @@ import interface_adapter.ViewManagerModel;
 import interface_adapter.home.HomeController;
 import interface_adapter.home.HomePresenter;
 import interface_adapter.search.SearchViewModel;
+import interface_adapter.view_response.ViewResponseViewModel;
 import use_case.home.HomeInputBoundary;
 import use_case.home.HomeInteractor;
 import use_case.home.HomeOutputBoundary;
+import views.ViewResponseView;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class ViewResponseViewUseCaseFactory {
     // reset response use case factory
-    private ViewResponseViewUseCaseFactory() {}
-
-    public static HomeController createResetUseCase(
-            ViewManagerModel viewManagerModel, SearchViewModel searchViewModel) throws IOException {
-        HomeOutputBoundary homeOutputBoundary = new HomePresenter(searchViewModel, viewManagerModel);
-        HomeInputBoundary homeInteractor = new HomeInteractor(homeOutputBoundary);
-        return new HomeController(homeInteractor);
+    private ViewResponseViewUseCaseFactory() {
     }
 
-    )
+    public static ViewResponseView create(
+            ViewManagerModel viewManagerModel, SearchViewModel searchViewModel,
+            ViewResponseViewModel viewResponseViewModel) {
+        try {
+            HomeController homeController = createResetUseCase(viewManagerModel, searchViewModel);
+            return new ViewResponseView(viewResponseViewModel, homeController);
 
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "something went wrong");
+        }
+        return null;
+    }
+
+        public static HomeController createResetUseCase (
+                ViewManagerModel viewManagerModel, SearchViewModel searchViewModel) throws IOException {
+
+            HomeOutputBoundary homeOutputBoundary = new HomePresenter(searchViewModel, viewManagerModel);
+            HomeInputBoundary homeInteractor = new HomeInteractor(homeOutputBoundary);
+            return new HomeController(homeInteractor);
+        }
 }
