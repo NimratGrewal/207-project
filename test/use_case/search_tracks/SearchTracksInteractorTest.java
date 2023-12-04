66cdb41b5757a61e266df8cbe820a40e8e478218
momoasalu <motunrayoasalu@gmail.com>
2023-12-04 16:28:01 -0500

    Add SearchTracksInteractorTest

--- test/use_case/search_tracks/SearchTracksInteractorTest.java
+++ test/use_case/search_tracks/SearchTracksInteractorTest.java
@@ -10,13 +10,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SearchTracksInteractorTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    SearchTracksOutputBoundary successPresenter = new SearchTracksOutputBoundary() {
+        @Override
+        public void prepareSuccessView(SearchTracksOutputData tracks) {
+            assertTrue(tracks.getTrackList().size() <= 5);
+        }
+    };
 
     @Test
     void prepareSuccess() {
         final SpotifyAPICaller c = new SpotifyAPICaller("216b6438ba554128ade0b63afa48ddd8", "56e33aff04bc4aa28dc3d9e54bb231cd");
 
+        SearchTracksInputBoundary sti = new SearchTracksInteractor(successPresenter, c);
+        SearchTracksInputData stid = new SearchTracksInputData("greedy");
+        sti.execute(stid);
     }
 }
